b77ee2ba3eea3b89de0ae9c72e06893ca23f2268
Thauzin | dev <thauanyfm3@gmail.com>
2024-05-24 10:12:40 -0300

    [ATT] - refazendo pagina produtos

--- script.js
+++ script.js
@@ -15,7 +15,37 @@ function Validar(){
 
 const btn = document.getElementById("car"); 
 
+// Seleciona os botões de tamanho pelo ID
+var pequeno = document.getElementById("p");
+var medio = document.getElementById("m");
+var grande = document.getElementById("g");
+
+// Variável para armazenar o tamanho selecionado
+var selectedSize = "";
+
 btn.addEventListener('click', ()=>{  
-    btn.textContent = "ADICIONADO" 
-    btn.style.backgroundColor = "red"; 
+        btn.textContent = "ADICIONADO" 
+        btn.style.backgroundColor = "#e6b845"; 
 }); 
+
+
+pequeno.addEventListener('click', ()=>{  
+    pequeno.style.backgroundColor = "#e6b845"; 
+    medio.style.backgroundColor = "#d8d8d8"; 
+    grande.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+medio.addEventListener('click', ()=>{  
+    medio.style.backgroundColor = "#e6b845"; 
+    pequeno.style.backgroundColor = "#d8d8d8"; 
+    grande.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+grande.addEventListener('click', ()=>{  
+    grande.style.backgroundColor = "#e6b845"; 
+    pequeno.style.backgroundColor = "#d8d8d8"; 
+    medio.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+
+
