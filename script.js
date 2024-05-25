c1173be5cb09acb6aafd49a3803b74e0af59afd3
Thauzin | dev <thauanyfm3@gmail.com>
2024-05-25 10:44:51 -0300

    [ATT] - primeira fileira finalizada

--- script.js
+++ script.js
@@ -47,5 +47,73 @@ grande.addEventListener('click', ()=>{
     medio.style.backgroundColor = "#d8d8d8"; 
 }); 
 
+var peq = document.getElementById("pp");
+var med = document.getElementById("mm");
+var gra = document.getElementById("gg");
+const btn2 = document.getElementById("card"); 
+
+// Variável para armazenar o tamanho selecionado
+var selectedSize = "";
+
+btn2.addEventListener('click', ()=>{  
+        btn2.textContent = "ADICIONADO" 
+        btn2.style.backgroundColor = "#e6b845"; 
+}); 
+
+
+peq.addEventListener('click', ()=>{  
+    peq.style.backgroundColor = "#e6b845"; 
+    med.style.backgroundColor = "#d8d8d8"; 
+    gra.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+med.addEventListener('click', ()=>{  
+    med.style.backgroundColor = "#e6b845"; 
+    peq.style.backgroundColor = "#d8d8d8"; 
+    gra.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+gra.addEventListener('click', ()=>{  
+    gra.style.backgroundColor = "#e6b845"; 
+    peq.style.backgroundColor = "#d8d8d8"; 
+    med.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+
+var pequ = document.getElementById("ppp");
+var medi = document.getElementById("mmm");
+var gran = document.getElementById("ggg");
+const btn3 = document.getElementById("cardi"); 
+
+// Variável para armazenar o tamanho selecionado
+var selectedSize = "";
+
+btn3.addEventListener('click', ()=>{  
+        btn3.textContent = "ADICIONADO" 
+        btn3.style.backgroundColor = "#e6b845"; 
+}); 
+
+
+pequ.addEventListener('click', ()=>{  
+    pequ.style.backgroundColor = "#e6b845"; 
+    medi.style.backgroundColor = "#d8d8d8"; 
+    gran.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+medi.addEventListener('click', ()=>{  
+    medi.style.backgroundColor = "#e6b845"; 
+    pequ.style.backgroundColor = "#d8d8d8"; 
+    gran.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+gran.addEventListener('click', ()=>{  
+    gran.style.backgroundColor = "#e6b845"; 
+    pequ.style.backgroundColor = "#d8d8d8"; 
+    medi.style.backgroundColor = "#d8d8d8"; 
+}); 
+
+
+
+
 
 
